7bc63c42bb19eeb3b4b4ca6c099046dc7ed85e44
Andreas Heige <a.heige.83@gmail.com>
2020-04-14 10:48:15 +0200

    added BE skeleton

---
+++ src/main/java/com/andreas/sbreactmysql/SbReactMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.andreas.sbreactmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbReactMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbReactMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andreas/sbreactmysql/SbReactMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreas.sbreactmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbReactMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
